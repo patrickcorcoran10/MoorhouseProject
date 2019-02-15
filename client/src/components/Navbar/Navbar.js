0c09520127200ecf8c5ba4e2c4fbf4fe02c10e0f
Patty C <patrickcorcoran10@gmail.com>
2019-02-15 10:14:45 -0600

    navbar enhancements

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,11 +4,23 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
     <div id='navbar' className='navbar'>
-      <Link to="/" className="navbar-brand">Dashboard</Link>
+      <ul class="nav nav-tabs">
+        <li class="nav-item">
+          <Link to="/" className="navbar-brand">Dashboard</Link>
+        </li>
+        <li class="nav-item">
           <Link to="/form" className="nav-link">Form</Link>
+        </li>
+        <li class="nav-item">
           <Link to="/view" className="nav-link">Company View</Link>
+        </li>
+        <li class="nav-item">
           <Link to="/display" className="nav-link">Display</Link>
+        </li>
+        <li class="nav-item">
           <Link to="/signin" className="nav-link">Log-Out</Link>
+        </li>
+      </ul>
     </div>
   )
 }
